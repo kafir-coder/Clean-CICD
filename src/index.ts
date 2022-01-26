b661e3e993fc1bee7778c8ab2d912dd0946c1b90
Caio Tony <caiotony16@gmail.com>
2022-01-26 14:31:29 +0100

    some push

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,7 @@ import { AppContainer } from "@/application/app";
 import {MONGODB_URI, PORT} from "@/application/config/environment";
 
 async function run(): Promise<void> {
+  console.log(MONGODB_URI)
   await connect(MONGODB_URI);
   console.log('DB Mongo connected')
   const app = await StartProjectInit.create(AppContainer);
